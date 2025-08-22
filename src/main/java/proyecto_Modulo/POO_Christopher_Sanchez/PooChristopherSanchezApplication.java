1bfae20db6c69905c3e94fc6e7517c6acc6bb681
Christopher Sanchez <diegsanchez27@gmail.com>
2025-08-22 10:28:53 -0600

    Primer commit

---
+++ src/main/java/proyecto_Modulo/POO_Christopher_Sanchez/PooChristopherSanchezApplication.java
@@ -0,0 +1,13 @@
+package proyecto_Modulo.POO_Christopher_Sanchez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PooChristopherSanchezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PooChristopherSanchezApplication.class, args);
+	}
+
+}
